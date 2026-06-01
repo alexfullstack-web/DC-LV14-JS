7420f003765ca15d77aee5de8e5eb5e223f430c4
Alex sousa <alexsousarodrigues1919@gmail.com>
2026-06-01 19:23:04 -0300

    envia atividade01

--- react/aula26/src/App.jsx
+++ react/aula26/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import acesa from './assets/acesa.png'
+import apagada from './assets/apagada.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+
+
   return (
     <>
-
+      <div id="aula26">
+        <h1>aula 26</h1>
+      </div>
+      <div id="atividade01"> 
+        <img src={count % 2 === 0 ? apagada : acesa} alt="Lâmpada" />
+        <button onClick={handleClick}>{count % 2 === 0 ? 'Acender' : 'Apagar'}</button>
+      </div>
     </>
   )
 }
