7c0162733fc0e714bbaea43e165413ff5e2ca396
Alex sousa <alexsousarodrigues1919@gmail.com>
2026-07-06 20:21:14 -0300

    adiciona nova linha em main.js para melhorar a formatação do código

--- node/aula35/main.js
+++ node/aula35/main.js
@@ -2,6 +2,7 @@ const { abrirConta, depositar, sacar, extrato } = require("./atividade01");
 
 let conta1 = abrirConta("João", "12345", "001", 1000);
 
+
 depositar(conta1, 500);
 sacar(conta1, 200);
 extrato(conta1);
@@ -9,3 +10,4 @@ extrato(conta1);
 console.log(
     `Conta de ${conta1.nome} - Número: ${conta1.numeroConta}, Agência: ${conta1.agencia}, Saldo: R$ ${conta1.saldo.toFixed(2)}`
 );
+
