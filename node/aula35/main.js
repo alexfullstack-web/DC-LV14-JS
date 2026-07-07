b7315eddbf67f0cde5222ad9520d6193c9d17115
Alex sousa <alexsousarodrigues1919@gmail.com>
2026-07-06 22:01:24 -0300

    refatora o código em main.js para melhorar a legibilidade e a estrutura

--- node/aula35/main.js
+++ node/aula35/main.js
@@ -1,15 +1,15 @@
-// const { abrirConta, depositar, sacar, extrato } = require("./atividade01");
+const { abrirConta, depositar, sacar, extrato } = require("./atividade01");
 
-// let conta1 = abrirConta("João", "12345", "001", 1000);
+let conta1 = abrirConta("João", "12345", "001", 1000);
 
 
-// depositar(conta1, 500);
-// sacar(conta1, 200);
-// extrato(conta1);
+depositar(conta1, 500);
+sacar(conta1, 200);
+extrato(conta1);
 
-// console.log(
-//     `Conta de ${conta1.nome} - Número: ${conta1.numeroConta}, Agência: ${conta1.agencia}, Saldo: R$ ${conta1.saldo.toFixed(2)}`
-// );
+console.log(
+    `Conta de ${conta1.nome} - Número: ${conta1.numeroConta}, Agência: ${conta1.agencia}, Saldo: R$ ${conta1.saldo.toFixed(2)}`
+);
 
 const{ lerArquivoJson, escreverArquivoJson, buscarObjeto } = require("./atividade03");
 
@@ -27,5 +27,3 @@ console.log(objeto);
 
 const objeto2 = buscarObjeto(caminhoArquivo, "frutas", 20);
 console.log(objeto2);
-
-
